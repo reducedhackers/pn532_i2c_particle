435d7f7b92a861405fcef9c23977a3e1c225686d
Nik Butler <nik@reducedhackers.com>
2016-06-28 11:52:32 +0000

    modify header include adding path to libraries

--- firmware/examples/readTag/readtag.ino
+++ firmware/examples/readTag/readtag.ino
@@ -1,6 +1,6 @@
-#include <pn532_i2c.h>
-#include <PN532.h>
-#include <NfcAdapter.h>
+#include "pn532_i2c\pn532_i2c.h"
+#include "pn532_i2c\PN532\PN532.h"
+#include "pn532_i2c\NDEF\NfcAdapter.h"
 
 PN532_I2C pn532_i2c(Wire);
 NfcAdapter nfc = NfcAdapter(pn532_i2c);
